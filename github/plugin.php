4b43fcbcdf4f940a6694115487a653ff04f591ea
Zachary Bloomquist <zbloomquist@fullambit.net>
2012-08-22 20:52:22 -0400

    Added readme, API func to plugin for hithub

--- github/plugin.php
+++ github/plugin.php
@@ -6,20 +6,39 @@ class plugin_github{
 		
 	}
 	
-	public function getTags($username,$repo){
-		$url = 'https://api.github.com/repos/'.$username.'/'.$repo.'/tags';
-				
-		$ch = curl_init($url);
-
-		curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
-		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-
-		$result = curl_exec($ch);
-		$result = json_decode($result,TRUE);
+	// internal function for contacting the github API
+	public function contactApi($call,$subdomain='api',$jsonEncode=TRUE) {
+		$url = 'https://' . $subdomain . '.github.com/' . $call;
+		if (function_exists('curl_init')) {
+			$ch = curl_init($url);
+			curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+			curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+			curl_setopt($ch,CURLOPT_USERAGENT,'SiteSense CMS https://github.com/FullAmbit/SiteSense'); // for stats
+			$result = curl_exec($ch);
+		} else {
+			$result = file_get_contents($url);
+		}
+		if (!$result) {
+			return FALSE; // something broke
+		}
+		if ($jsonEncode) {
+			$result = json_decode($result,TRUE);
+		}
 		curl_close($ch);
-		
 		return $result;
 	}
+	
+	public function getTags($username,$repo){
+		return contactApi('repos/'.$username.'/'.$repo.'/tags');
+	}
+	
+	public function getReadme($username,$repo){
+		return contactApi($username . '/' . $repo . '/master/README.md','raw',FALSE);
+	}
+	
+	// TODO parse out the markdown in the readme into some sort of array where we can see
+	// the information we need to classify their plugin, like:
+	// http://wordpress.org/extend/plugins/about/readme.txt
 	
 }
 ?>
