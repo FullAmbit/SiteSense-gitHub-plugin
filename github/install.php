d4b22680fcb72ccc3552b61848c751d18ce1de09
Zachary Bloomquist <zbloomquist@fullambit.net>
2012-08-23 20:52:49 -0400

    Added dynamic form with parser to submit customizations, works with github too

---
+++ github/install.php
@@ -0,0 +1,14 @@
+<?php
+function github_settings() {
+	return array(
+		'isCDN' => 1,
+		'isEditor' => 0
+	);
+}
+function github_install($data,$db) {
+	$data->output['installSuccess'] = TRUE;
+}
+function github_uninstall($data,$db) {
+	$data->output['uninstallSuccess'] = TRUE;
+}
+?>
--- github/plugin.php
+++ github/plugin.php
@@ -29,18 +29,31 @@ class plugin_github{
 		return $result;
 	}
 	
+	public function getRepo($username,$repo) {
+		return $this->contactApi('repos/'.$username.'/'.$repo);
+	}
+	
 	public function getTags($username,$repo){
 		return $this->contactApi('repos/'.$username.'/'.$repo.'/tags');
 	}
 	
 	public function getReadme($username,$repo){
-		return $this->contactApi($username . '/' . $repo . '/master/README.md','raw',FALSE);
+		//return $this->contactApi($username . '/' . $repo . '/master/README.md','raw',FALSE);
+		$readmeArray = $this->contactApi('repos/'.$username . '/' . $repo . '/contents/README.md');
+		if (isset($readmeArray['content'])) {
+			return base64_decode($readmeArray['content']);
+		} else {
+			return FALSE;
+		}
 	}
 	
 	// parses the readme into array according to this basic format
 	// see https://raw.github.com/flotwig/sample-sitesense/master/README.md
 	public function parseReadme($username,$repo){
 		$raw = $this->getReadme($username,$repo);
+		if (!$raw) {
+			return FALSE;
+		}
 		$raw = explode("\n",$raw);
 		$returnArray = array();
 		foreach ($raw as $readmeLine) {
@@ -57,10 +70,16 @@ class plugin_github{
 			}
 		}
 		// now let's make sure we have all of the required infos
-		$requiredKeys = array('name','author','website','tags','requires','tested','stable','license');
+		$requiredKeys = array('name','author','description','website','tags','requires','tested','stable','license');
 		foreach ($requiredKeys as $requiredKey) {
 			if (!array_key_exists($requiredKey,$returnArray)) {
 				return false;
+			}
+		}
+		// trim the fat
+		foreach ($returnArray as $returnKey=>$returnValue) {
+			if (!in_array($returnKey,$requiredKeys)) {
+				unset($returnArray[$returnKey]);
 			}
 		}
 		return $returnArray;
